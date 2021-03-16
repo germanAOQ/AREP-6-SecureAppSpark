47ea3b499f5e81932170d9f45ec875292f9227f9
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-03-16 16:32:15 -0500

    Connection with other service

--- src/main/java/edu/escuelaing/arep/securespark/connection/SecureURLReader.java
+++ src/main/java/edu/escuelaing/arep/securespark/connection/SecureURLReader.java
@@ -1,4 +1,4 @@
-package edu.escuelaing.arep.securespark;
+package edu.escuelaing.arep.securespark.connection;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
@@ -37,14 +37,33 @@ public class SecureURLReader {
         SSLContext.setDefault(sslContext);
 
         // We can now read this URL
-        readURL("https://localhost:5000/helloservice");
+        //readURL("https://localhost:5000/actualtime");
 
         // This one can't be read because the Java default truststore has been
         // changed.
-        readURL("https://www.google.com");
+        //readURL("https://www.google.com");
     }
 
-    private static void readURL(String site) throws IOException {
+    public static String readURL(String site) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, KeyManagementException {
+        File trustStoreFile = new File("keystores/myTrustStore");
+        char[] trustStorePassword = "654321".toCharArray();
+
+        // Load the trust store, the default type is "pkcs12", the alternative is "jks"
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(new FileInputStream(trustStoreFile), trustStorePassword);
+
+        // Get the singleton instance of the TrustManagerFactory
+        TrustManagerFactory tmf = TrustManagerFactory
+                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+        // Itit the TrustManagerFactory using the truststore object
+        tmf.init(trustStore);
+
+        //Set the default global SSLContext so all the connections will use it
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+        SSLContext.setDefault(sslContext);
+
         // Crea el objeto que representa una URL
         URL siteURL = new URL(site);
         // Crea el objeto que URLConnection
@@ -54,27 +73,32 @@ public class SecureURLReader {
         // Obtiene una vista del mapa como conjunto de pares <K,V> // para poder navegarlo
         Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
         // Recorre la lista de campos e imprime los valores
+        String inputLine = null;
+        String res = null;
         for (Map.Entry<String, List<String>> entry : entrySet) {
             String headerName = entry.getKey();
             //Si el nombre es nulo, significa que es la linea de estado
             if (headerName != null) {
-                System.out.print(headerName + ":");
+                //System.out.print(headerName + ":");
             }
             List<String> headerValues = entry.getValue();
             for (String value : headerValues) {
-                System.out.print(value);
+                //System.out.print(value);
             }
-            System.out.println("");
+            //System.out.println("");
         }
         try {
             BufferedReader reader
                     = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            String inputLine = null;
             while ((inputLine = reader.readLine()) != null) {
-                System.out.println(inputLine);
+                //System.out.println(inputLine);
+                if(inputLine != null){
+                    res = inputLine;
+                }
             }
         } catch (IOException x) {
             System.err.println(x);
         }
+        return res;
     }
 }
--- src/main/java/edu/escuelaing/arep/securespark/service/HelloSecureService.java
+++ src/main/java/edu/escuelaing/arep/securespark/service/HelloSecureService.java
@@ -1,14 +1,25 @@
 package edu.escuelaing.arep.securespark.service;
+
+import com.google.gson.Gson;
+
+import java.time.LocalDate;
+
 import static spark.Spark.*;
+import static edu.escuelaing.arep.securespark.connection.SecureURLReader.readURL;
 
 public class HelloSecureService {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
         secure("keystores/ecikeystore.p12", "123456", "keystores/myTrustStore", "654321");
         port(getPort());
-        get("/helloservice", (req,res)->{
+        staticFiles.location("/public");
+        get("/helloservice", (req, res) -> {
             return "Hello from secure service";
+        });
+        get("/actualtime", (req, res) -> {
+            LocalDate localdate = new Gson().fromJson(readURL("https://localhost:4567/actualtime"), LocalDate.class);
+            return new Gson().toJson(localdate);
         });
     }
 
